05686e5dd8222906a348b36e4736732086f0a24b
nevgoodyer <45184841+nevgoodyer@users.noreply.github.com>
2019-04-23 19:01:02 +1000

    Menus working

--- Bounce/frmBounce.vb
+++ Bounce/frmBounce.vb
@@ -1,3 +1,13 @@
 ﻿Public Class frmBounce
+    Private Sub ColourToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColourToolStripMenuItem.Click
+        frmColour.Show()
+    End Sub
 
+    Private Sub SpeedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SpeedToolStripMenuItem.Click
+        frmSpeed.Show()
+    End Sub
+
+    Private Sub SizeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SizeToolStripMenuItem.Click
+        frmSize.Show()
+    End Sub
 End Class
